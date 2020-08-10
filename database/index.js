028b5279f694588fc7da79062a2f980ef343d098
Delfos <rafael.costa@delfosim.com>
2020-08-10 20:22:04 -0300

    database url from env

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg')
 
 export const client = new Client({
-  connectionString: "postgres://ppewmoakgfiumc:0cd47a74912697b79fece86171be6e45c8050a4d2a4f10f09d674896bc6ee4ab@ec2-18-211-48-247.compute-1.amazonaws.com:5432/dao9or2k8hacds",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
